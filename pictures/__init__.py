ba4d46954eb150284cdb5d0fae0b3b96fdd1d31b
Hectic  coder <161811788+Hello-Mr-Crab@users.noreply.github.com>
2024-11-02 09:59:42 +0800

    Update __init__.py

--- pictures/__init__.py
+++ pictures/__init__.py
@@ -2,4 +2,4 @@
 #Author：Mr.Crab
 
 '''This folder is used to store all the base64 picture pyfile'''
-'''please Don't move or delete any pyfile in this folder'''
+'''please Don't move or delete any pyfile in this folder!'''
