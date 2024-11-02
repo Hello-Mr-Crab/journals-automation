415d92e157be337fd849fc028e2851c1e1beec6f
Hectic  coder <161811788+Hello-Mr-Crab@users.noreply.github.com>
2024-11-02 09:50:22 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,2 +1,16 @@
-#version:8.0
+#Version:8.0
 #Author:Mr.Crab
+#项目结构:
+'''
+论文自动化助手_ __
+                 |
+                 |_ _ _pictures_ _ _
+                 |              |_ _ _Background:存放背景图片的文件夹(py文件)
+                 |              | _ _ _Icons：存放Icon的文件夹(py文件
+                 |_ _ _utils_ _ _
+                 |           |_ _ _ system settings.py:windows系统设置的一些函数
+                 |           |_ _ _Toools.py:一些有用的函数
+                 |
+                 |_ _ _论文自动化助手.py：main.py
+                 |_ _ _论文自动化助手(打包用)：所有py文件都在该文件夹内，适合打包
+'''
